388c71edb8d35d92c196cb420521b2a3ebd49280
RomyToklio <contact@evaveo.com>
2018-07-25 16:33:58 +0200

    Update

--- translations/monero-core_hi.ts
+++ translations/monero-core_hi.ts
@@ -20,7 +20,7 @@
     </message>
     <message>
         <location filename="../pages/AddressBook.qml" line="66"/>
-        <source>4.. / 8..</source>
+        <source>PYX...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
