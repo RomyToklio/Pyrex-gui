8a6834299d8a00253a226fe9e183c01188a89c89
RomyToklio <contact@evaveo.com>
2018-11-27 10:59:49 +0100

    update

--- build.sh
+++ build.sh
@@ -98,11 +98,11 @@ fi
 
 # force version update
 get_tag
-#echo "var GUI_VERSION = \"$TAGNAME\"" > version.js
+echo "var GUI_VERSION = \"$TAGNAME\"" > version.js
 pushd "$MONERO_DIR"
 get_tag
 popd
-#echo "var GUI_MONERO_VERSION = \"$TAGNAME\"" >> version.js
+echo "var GUI_MONERO_VERSION = \"$TAGNAME\"" >> version.js
 
 cd build
 if ! QMAKE=$(find_command qmake qmake-qt5); then
