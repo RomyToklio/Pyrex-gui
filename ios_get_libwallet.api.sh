8612cf557e971994a422415e69c9b154232c2812
Romy <16287732+RomyToklio@users.noreply.github.com>
2019-04-03 09:42:57 +0200

    Update ios_get_libwallet.api.sh

--- ios_get_libwallet.api.sh
+++ ios_get_libwallet.api.sh
@@ -25,31 +25,31 @@ if [ -z $INSTALL_PREFIX ]; then
 fi
 
 echo "Building IOS armv7"
-rm -r monero/build > /dev/null
-mkdir -p monero/build/release
-pushd monero/build/release
+rm -r Pyrex/build > /dev/null
+mkdir -p Pyrex/build/release
+pushd Pyrex/build/release
 cmake -D IOS=ON -D ARCH=armv7 -D BOOST_LIBRARYDIR=${BOOST_LIBRARYDIR} -D BOOST_INCLUDEDIR=${BOOST_INCLUDEDIR} -D OPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR} -D OPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} -D CMAKE_BUILD_TYPE=debug -D STATIC=ON -D BUILD_GUI_DEPS=ON -D INSTALL_VENDORED_LIBUNBOUND=ON -D SODIUM_LIBRARY=$SODIUM_LIBRARY -D SODIUM_INCLUDE_DIR=$SODIUM_INCLUDE -D CMAKE_INSTALL_PREFIX=$INSTALL_PREFIX  ../..
 make -j4 && make install
 popd
 
 echo "Building IOS arm64"
-rm -r monero/build > /dev/null
-mkdir -p monero/build/release
-pushd monero/build/release
+rm -r Pyrex/build > /dev/null
+mkdir -p Pyrex/build/release
+pushd Pyrex/build/release
 cmake -D IOS=ON -D ARCH=arm64 -D BOOST_LIBRARYDIR=${BOOST_LIBRARYDIR} -D BOOST_INCLUDEDIR=${BOOST_INCLUDEDIR} -D OPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR} -D OPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} -D CMAKE_BUILD_TYPE=debug -D STATIC=ON -D BUILD_GUI_DEPS=ON -D INSTALL_VENDORED_LIBUNBOUND=ON -D SODIUM_LIBRARY=$SODIUM_LIBRARY -D SODIUM_INCLUDE_DIR=$SODIUM_INCLUDE -D CMAKE_INSTALL_PREFIX=$INSTALL_PREFIX  ../..
 make -j4 && make install
 popd
 
 echo "Building IOS x86"
-rm -r monero/build > /dev/null
-mkdir -p monero/build/release
-pushd monero/build/release
+rm -r Pyrex/build > /dev/null
+mkdir -p Pyrex/build/release
+pushd Pyrex/build/release
 cmake -D IOS=ON -D ARCH=x86_64 -D IOS_PLATFORM=SIMULATOR64 -D BOOST_LIBRARYDIR=${BOOST_LIBRARYDIR_x86_64} -D BOOST_INCLUDEDIR=${BOOST_INCLUDEDIR} -D OPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR} -D OPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} -D CMAKE_BUILD_TYPE=debug -D STATIC=ON -D BUILD_GUI_DEPS=ON -D INSTALL_VENDORED_LIBUNBOUND=ON -D SODIUM_LIBRARY=$SODIUM_LIBRARY -D SODIUM_INCLUDE_DIR=$SODIUM_INCLUDE -D CMAKE_INSTALL_PREFIX=$INSTALL_PREFIX  ../..
 make -j4 && make install
 popd
 
 echo "Creating fat library for armv7 and arm64"
-pushd monero
+pushd Pyrex
 mkdir -p lib-ios
 lipo -create lib-armv7/libwallet_merged.a lib-x86_64/libwallet_merged.a lib-armv8-a/libwallet_merged.a -output lib-ios/libwallet_merged.a
 lipo -create lib-armv7/libunbound.a lib-x86_64/libunbound.a lib-armv8-a/libunbound.a -output lib-ios/libunbound.a
